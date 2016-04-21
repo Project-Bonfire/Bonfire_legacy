441c588e14210c20487eec10080b424312a8b8aa
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2016-04-21 09:50:42 +0300

    added network size to LBDR interface... the rest can happen inside LBDR itself

--- RTL/LBDR.vhd
+++ RTL/LBDR.vhd
@@ -9,22 +9,23 @@ entity LBDR is
     generic (
         cur_addr_rst: integer := 8;
         Rxy_rst: integer := 8;
-        Cx_rst: integer := 8
+        Cx_rst: integer := 8;
+        NoC_size: integer := 4
     );
     port (  reset: in  std_logic;
             clk: in  std_logic;
             empty: in  std_logic;
             flit_type: in std_logic_vector(2 downto 0);
-            dst_addr: in std_logic_vector(3 downto 0);
+            dst_addr: in std_logic_vector(NoC_size-1 downto 0);
             Req_N, Req_E, Req_W, Req_S, Req_L:out std_logic
             );
 end LBDR;
 
 architecture behavior of LBDR is
 
-  signal Cx:  std_logic_vector(3 downto 0);
+  signal Cx:  std_logic_vector(NoC_size-1 downto 0);
   signal Rxy:  std_logic_vector(7 downto 0);
-  signal cur_addr:  std_logic_vector(3 downto 0);  
+  signal cur_addr:  std_logic_vector(NoC_size-1 downto 0);  
   signal N1, E1, W1, S1  :std_logic :='0';  
   signal Req_N_in, Req_E_in, Req_W_in, Req_S_in, Req_L_in: std_logic;
   signal Req_N_FF, Req_E_FF, Req_W_FF, Req_S_FF, Req_L_FF: std_logic;
@@ -34,10 +35,10 @@ begin
   Rxy      <= std_logic_vector(to_unsigned(Rxy_rst, Rxy'length));
   cur_addr <= std_logic_vector(to_unsigned(cur_addr_rst, cur_addr'length));
 
-  N1 <= '1' when  dst_addr(3 downto 2) < cur_addr(3 downto 2) else '0';
+  N1 <= '1' when  dst_addr(NoC_size-1 downto 2) < cur_addr(NoC_size-1 downto 2) else '0';
   E1 <= '1' when  cur_addr(1 downto 0) < dst_addr(1 downto 0) else '0';
   W1 <= '1' when  dst_addr(1 downto 0) < cur_addr(1 downto 0) else '0';
-  S1 <= '1' when  cur_addr(3 downto 2) < dst_addr(3 downto 2) else '0';
+  S1 <= '1' when  cur_addr(NoC_size-1 downto 2) < dst_addr(NoC_size-1 downto 2) else '0';
 
 
 process(clk, reset)
