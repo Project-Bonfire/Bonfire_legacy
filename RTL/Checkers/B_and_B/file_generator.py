51078e142dccc0aba035120ace733aa44e501514
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2016-06-05 10:25:20 +0300

    Im trying to focus on one problem! :P

--- RTL/Checkers/B_and_B/file_generator.py
+++ RTL/Checkers/B_and_B/file_generator.py
@@ -2,7 +2,7 @@
 
 import os
 import package_file
-from vhdl_generators import gen_lbdr_cheker_files, gen_elbdr_cheker_files, gen_elbdr_checker_top
+from vhdl_generators import gen_elbdr_cheker_files, gen_elbdr_checker_top
 from script_generators import gen_elbdr_synth_script
 
 
@@ -28,22 +28,15 @@ def generate_specific_file(current_selected_list):
     if len(current_selected_list) == 0:
         return None
 
-    if package_file.unit_under_test == "LBDR":
-        gen_lbdr_cheker_files.gen_lbdr_checkers(current_selected_list)
-
-    elif package_file.unit_under_test == "ELBDR":
+    if package_file.unit_under_test == "ELBDR":
         gen_elbdr_cheker_files.gen_elbdr_checkers(current_selected_list)
         gen_elbdr_checker_top.gen_elbdr_checker_top(current_selected_list)
         gen_elbdr_synth_script.gen_elbdr_checker_top(current_selected_list)
 
 
 def generate_initial_files():
-    if package_file.unit_under_test == "LBDR":
-        for i in package_file.list_of_checkers:
-            list_of_checkers = [i]
-            gen_lbdr_cheker_files.gen_lbdr_checkers(list_of_checkers)
 
-    elif package_file.unit_under_test == "ELBDR":
+    if package_file.unit_under_test == "ELBDR":
         for i in package_file.list_of_checkers:
             list_of_checkers = [i]
             gen_elbdr_cheker_files.gen_elbdr_checkers(list_of_checkers)
