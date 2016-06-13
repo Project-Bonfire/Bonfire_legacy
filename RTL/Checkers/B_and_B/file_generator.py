671905f703856970b4da481f6efd5db9c3571008
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2016-06-13 15:21:12 +0300

    file ten update

--- RTL/Checkers/B_and_B/file_generator.py
+++ RTL/Checkers/B_and_B/file_generator.py
@@ -4,6 +4,7 @@ import os
 
 import package_file
 from vhdl_generators import gen_elbdr_cheker_files, gen_elbdr_checker_top, gen_lbdr_cheker_files, gen_lbdr_checker_top
+from vhdl_generators import gen_fifo_checker_top, gen_fifo_cheker_files
 import gen_unit_synth_script
 
 
@@ -47,8 +48,8 @@ def generate_specific_file(current_selected_list):
         gen_unit_synth_script.gen_unit_checker_script(current_selected_list)
 
     if package_file.unit_under_test == "FIFO":
-        gen_lbdr_cheker_files.gen_lbdr_checkers(current_selected_list)
-        gen_lbdr_checker_top.gen_lbdr_checker_top(current_selected_list)
+        gen_fifo_cheker_files.gen_fifo_checkers(current_selected_list)
+        gen_fifo_checker_top.gen_fifo_checker_top(current_selected_list)
         gen_unit_synth_script.gen_unit_checker_script(current_selected_list)
 
 
@@ -70,6 +71,6 @@ def generate_initial_files():
     if package_file.unit_under_test == "FIFO":
         for i in package_file.list_of_checkers:
             list_of_checkers = [i]
-            gen_lbdr_cheker_files.gen_lbdr_checkers(list_of_checkers)
-            gen_lbdr_checker_top.gen_lbdr_checker_top(list_of_checkers)
+            gen_fifo_cheker_files.gen_fifo_checkers(list_of_checkers)
+            gen_fifo_checker_top.gen_fifo_checker_top(list_of_checkers)
             gen_unit_synth_script.gen_unit_checker_script(list_of_checkers)
