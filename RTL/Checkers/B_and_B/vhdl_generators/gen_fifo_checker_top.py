51d9585fdc597f575649547f523b11a0ea960b86
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2016-06-14 17:50:59 +0300

    script update

--- RTL/Checkers/B_and_B/vhdl_generators/gen_fifo_checker_top.py
+++ RTL/Checkers/B_and_B/vhdl_generators/gen_fifo_checker_top.py
@@ -5,7 +5,7 @@ from misc import name_string_generator
 def gen_fifo_checker_top(checker_id):
     name_string = name_string_generator(checker_id)
     
-    fifo_checker_top = open("checker_vhdl/fifo_checker"+name_string+"_top.vhd", 'w')
+    fifo_checker_top = open("checker_vhdl/fifo_control_part_checker"+name_string+"_top.vhd", 'w')
 
     fifo_checker_top.write("library ieee;\n")
     fifo_checker_top.write("use ieee.std_logic_1164.all;\n")
