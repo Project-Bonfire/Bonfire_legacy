d0610d53bba929c0be612550ec44a910468a8a40
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2016-06-21 13:36:18 +0300

    Update greedy_search.py

--- RTL/Checkers/Greedy/greedy_search.py
+++ RTL/Checkers/Greedy/greedy_search.py
@@ -191,29 +191,38 @@ else:
 print "------------------------------"
 print "starting optimization with ", current_list, "checkers already chosen"
 print "and running greedy algorithm on ", checkers_for_optimization
-if check_feasibility(current_list[1:], current_list[0]):
-    print "starting sequence is feasible...  going further..."
-    coverage = calculate_coverage(current_list)
-    print "starting coverage:", coverage
-    best_coverage = coverage
-    for item in sorted_coverage:
-        if item[0] in checkers_for_optimization:
-            print "------------------------------"
-            print "Trying picking item:", item[0]
-            if item not in current_list:
-                if check_feasibility(current_list, item[0]):
-                    list_for_coverage_check = current_list+[item[0]]
-                    coverage = calculate_coverage(list_for_coverage_check)
-                    print "coverage:", coverage
-                    if coverage > best_coverage:
-                        current_list.append(item[0])
-                        best_coverage = coverage
-                        print "coverage improved to:", coverage
-                    else:
-                        print "\033[91m* NOTE::\033[0m No improvement is made. ignoring the item..."
-                    if coverage == 100:
-                        print "100% coverage reached! ending the search!"
-                        break
+
+if len(current_list) > 0:
+	if check_feasibility(current_list[1:], current_list[0]):
+		print "starting sequence is feasible...  going further..."
+		coverage = calculate_coverage(current_list)
+		print "starting coverage:", coverage
+	else:
+		print "starting sequence is not feasible... breaking"
+		sys.exit()
+else:
+	coverage = 0
+	print "starting coverage:", coverage
+
+best_coverage = coverage
+for item in sorted_coverage:
+    if item[0] in checkers_for_optimization:
+        print "------------------------------"
+        print "Trying picking item:", item[0]
+        if item not in current_list:
+            if check_feasibility(current_list, item[0]):
+                list_for_coverage_check = current_list+[item[0]]
+                coverage = calculate_coverage(list_for_coverage_check)
+                print "coverage:", coverage
+                if coverage > best_coverage:
+                    current_list.append(item[0])
+                    best_coverage = coverage
+                    print "coverage improved to:", coverage
+                else:
+                    print "\033[91m* NOTE::\033[0m No improvement is made. ignoring the item..."
+                if coverage == 100:
+                    print "100% coverage reached! ending the search!"
+                    break
 
 print "------------------------------"
 print "\033[32m* NOTE::\033[0m best solution:", current_list
