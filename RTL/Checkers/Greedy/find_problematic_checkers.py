65cc9ed4b82757773ab4417e8765465eea8f474a
siavooshpayandehazad <siavooshpayandeh@gmail.com>
2016-07-01 18:23:08 +0300

    still bug fixing

--- RTL/Checkers/Greedy/find_problematic_checkers.py
+++ RTL/Checkers/Greedy/find_problematic_checkers.py
@@ -15,15 +15,12 @@ def find_problematic_checkers():
         for item_2 in range(1, package_file.number_of_checkers+1):
                 if item_1 != item_2:
                     if (item_1, item_2) not in checked_checkers:
-                        if (item_2, item_1) not in checked_checkers:
-                            checked_checkers.append((item_1, item_2))
-
-                            generate_specific_file([item_1])
-                            calculate_area([item_1, item_2])
-                            new_coverage = calculate_coverage([item_1, item_2])
-
-                            if new_coverage == initial_coverage:
-                                print "PROBLEMATIC SET OF CHECKERS DETECTED:", item_2," IS DOMINATED BY", item_1
-                                problematic_checker_detected = True
+                        checked_checkers.append((item_1, item_2))
+                        generate_specific_file([item_1])
+                        calculate_area([item_1, item_2])
+                        new_coverage = calculate_coverage([item_1, item_2])
+                        if new_coverage == initial_coverage:
+                            print "\033[91m* NOTE::\033[0m PROBLEMATIC SET OF CHECKERS DETECTED:", item_2," IS DOMINATED BY", item_1
+                            problematic_checker_detected = True
     return problematic_checker_detected
 
