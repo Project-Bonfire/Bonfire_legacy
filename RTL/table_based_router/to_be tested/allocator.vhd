0d9266d7ff70c06fcae5d561333753fd7169a063
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2016-07-12 11:44:24 +0200

    cleanup

--- RTL/table_based_router/to_be tested/allocator.vhd
+++ RTL/table_based_router/to_be tested/allocator.vhd
@@ -12,79 +12,23 @@ entity allocator is       -- table based routing module
     port (  reset: in  std_logic;
             clk: in  std_logic;
             -- flow control
-            credit_in_N: in std_logic;
-            credit_in_E: in std_logic;
-            credit_in_W: in std_logic;
-            credit_in_S: in std_logic;
-            credit_in_L: in std_logic;
+            credit_in_N, credit_in_E, credit_in_W, credit_in_S, credit_in_L: in std_logic;
 
-           	req_N_N: in std_logic;
-           	req_N_E: in std_logic;
-           	req_N_W: in std_logic;
-           	req_N_S: in std_logic;
-           	req_N_L: in std_logic;
-
-           	req_E_N: in std_logic;
-           	req_E_E: in std_logic;
-           	req_E_W: in std_logic;
-           	req_E_S: in std_logic;
-           	req_E_L: in std_logic;
-
-           	req_W_N: in std_logic;
-           	req_W_E: in std_logic;
-           	req_W_W: in std_logic;
-           	req_W_S: in std_logic;
-           	req_W_L: in std_logic;
-
-           	req_S_N: in std_logic;
-           	req_S_E: in std_logic;
-           	req_S_W: in std_logic;
-           	req_S_S: in std_logic;
-           	req_S_L: in std_logic;
-
-           	req_L_N: in std_logic;
-           	req_L_E: in std_logic;
-           	req_L_W: in std_logic;
-           	req_L_S: in std_logic;
-           	req_L_L: in std_logic;
+           	req_N_N, req_N_E, req_N_W, req_N_S, req_N_L: in std_logic;
+           	req_E_N, req_E_E, req_E_W, req_E_S, req_E_L: in std_logic;
+           	req_W_N, req_W_E, req_W_W, req_W_S, req_W_L: in std_logic;
+           	req_S_N, req_S_E, req_S_W, req_S_S, req_S_L: in std_logic;
+           	req_L_N, req_L_E, req_L_W, req_L_S, req_L_L: in std_logic;
 
            	-- grant_X_Y means the grant for X output port towards Y input port
            	-- this means for any X in [N, E, W, S, L] then set grant_X_Y is one hot!
-            valid_N : out std_logic;
-            valid_E : out std_logic;
-            valid_W : out std_logic;
-            valid_S : out std_logic;
-            valid_L : out std_logic;
+            valid_N, valid_E, valid_W, valid_S, valid_L : out std_logic;
 
-           	grant_N_N: out std_logic;
-           	grant_N_E: out std_logic;
-           	grant_N_W: out std_logic;
-           	grant_N_S: out std_logic;
-           	grant_N_L: out std_logic;
-
-           	grant_E_N: out std_logic;
-           	grant_E_E: out std_logic;
-           	grant_E_W: out std_logic;
-           	grant_E_S: out std_logic;
-           	grant_E_L: out std_logic;
-
-           	grant_W_N: out std_logic;
-           	grant_W_E: out std_logic;
-           	grant_W_W: out std_logic;
-           	grant_W_S: out std_logic;
-           	grant_W_L: out std_logic;
-
-           	grant_S_N: out std_logic;
-           	grant_S_E: out std_logic;
-           	grant_S_W: out std_logic;
-           	grant_S_S: out std_logic;
-           	grant_S_L: out std_logic;
-
-           	grant_L_N: out std_logic;
-           	grant_L_E: out std_logic;
-           	grant_L_W: out std_logic;
-           	grant_L_S: out std_logic;
-           	grant_L_L: out std_logic
+           	grant_N_N, grant_N_E, grant_N_W, grant_N_S, grant_N_L: out std_logic;
+           	grant_E_N, grant_E_E, grant_E_W, grant_E_S, grant_E_L: out std_logic;
+           	grant_W_N, grant_W_E, grant_W_W, grant_W_S, grant_W_L: out std_logic;
+           	grant_S_N, grant_S_E, grant_S_W, grant_S_S, grant_S_L: out std_logic;
+           	grant_L_N, grant_L_E, grant_L_W, grant_L_S, grant_L_L: out std_logic
             );
 end allocator;
 
@@ -203,26 +147,22 @@ process(credit_in_N, credit_in_E, credit_in_W, credit_in_S, credit_in_L, grant_N
 
 
 arb_N_X: arbiter_in  PORT MAP (Req_X_N=>req_N_N, Req_X_E=> req_N_E, Req_X_W=>req_N_W, Req_X_S=>req_N_S, Req_X_L=>req_N_L,
-                          X_N=>X_N_N, X_E=>X_N_E, X_W=>X_N_W, X_S=>X_N_S, X_L=>X_N_L);
+                               X_N=>X_N_N, X_E=>X_N_E, X_W=>X_N_W, X_S=>X_N_S, X_L=>X_N_L);
 
 arb_E_X: arbiter_in  PORT MAP (Req_X_N=>req_E_N, Req_X_E=> req_E_E, Req_X_W=>req_E_W, Req_X_S=>req_E_S, Req_X_L=>req_E_L,
-                          X_N=>X_E_N, X_E=>X_E_E, X_W=>X_E_W, X_S=>X_E_S, X_L=>X_E_L);
+                               X_N=>X_E_N, X_E=>X_E_E, X_W=>X_E_W, X_S=>X_E_S, X_L=>X_E_L);
 
 arb_W_X: arbiter_in  PORT MAP (Req_X_N=>req_W_N, Req_X_E=> req_W_E, Req_X_W=>req_W_W, Req_X_S=>req_W_S, Req_X_L=>req_W_L,
-                          X_N=>X_W_N, X_E=>X_W_E, X_W=>X_W_W, X_S=>X_W_S, X_L=>X_W_L);
+                               X_N=>X_W_N, X_E=>X_W_E, X_W=>X_W_W, X_S=>X_W_S, X_L=>X_W_L);
 
 arb_S_X: arbiter_in  PORT MAP (Req_X_N=>req_S_N, Req_X_E=> req_S_E, Req_X_W=>req_S_W, Req_X_S=>req_S_S, Req_X_L=>req_S_L,
-                          X_N=>X_S_N, X_E=>X_S_E, X_W=>X_S_W, X_S=>X_S_S, X_L=>X_S_L);
+                               X_N=>X_S_N, X_E=>X_S_E, X_W=>X_S_W, X_S=>X_S_S, X_L=>X_S_L);
 
 arb_L_X: arbiter_in  PORT MAP (Req_X_N=>req_L_N, Req_X_E=> req_L_E, Req_X_W=>req_L_W, Req_X_S=>req_L_S, Req_X_L=>req_L_L,
-                          X_N=>X_L_N, X_E=>X_L_E, X_W=>X_L_W, X_S=>X_L_S, X_L=>X_L_L);
+                               X_N=>X_L_N, X_E=>X_L_E, X_W=>X_L_W, X_S=>X_L_S, X_L=>X_L_L);
 
 -- Y is N now
-arb_X_N: arbiter_out port map (X_N_Y => X_N_N, 
-                               X_E_Y => X_E_N , 
-                               X_W_Y => X_W_N, 
-                               X_S_Y => X_S_N, 
-                               X_L_Y => X_L_N, 
+arb_X_N: arbiter_out port map (X_N_Y => X_N_N, X_E_Y => X_E_N,  X_W_Y => X_W_N,  X_S_Y => X_S_N,  X_L_Y => X_L_N, 
                                credit => credit_counter_N_out,
                                grant_Y_N => grant_N_N_sig, 
                                grant_Y_E => grant_N_E_sig, 
@@ -231,11 +171,7 @@ arb_X_N: arbiter_out port map (X_N_Y => X_N_N,
                                grant_Y_L => grant_N_L_sig);
 
 -- Y is E now
-arb_X_E: arbiter_out port map (X_N_Y => X_N_E, 
-                               X_E_Y => X_E_E , 
-                               X_W_Y => X_W_E, 
-                               X_S_Y => X_S_E, 
-                               X_L_Y => X_L_E, 
+arb_X_E: arbiter_out port map (X_N_Y => X_N_E, X_E_Y => X_E_E, X_W_Y => X_W_E, X_S_Y => X_S_E, X_L_Y => X_L_E, 
                                credit => credit_counter_E_out,
                                grant_Y_N => grant_E_N_sig, 
                                grant_Y_E => grant_E_E_sig, 
@@ -244,11 +180,7 @@ arb_X_E: arbiter_out port map (X_N_Y => X_N_E,
                                grant_Y_L => grant_E_L_sig);
 
 -- Y is W now
-arb_X_W: arbiter_out port map (X_N_Y => X_N_W, 
-                               X_E_Y => X_E_W , 
-                               X_W_Y => X_W_W, 
-                               X_S_Y => X_S_W, 
-                               X_L_Y => X_L_W, 
+arb_X_W: arbiter_out port map (X_N_Y => X_N_W, X_E_Y => X_E_W, X_W_Y => X_W_W, X_S_Y => X_S_W, X_L_Y => X_L_W, 
                                credit => credit_counter_W_out,
                                grant_Y_N => grant_W_N_sig, 
                                grant_Y_E => grant_W_E_sig, 
@@ -257,11 +189,7 @@ arb_X_W: arbiter_out port map (X_N_Y => X_N_W,
                                grant_Y_L => grant_W_L_sig);
 
 -- Y is S now
-arb_X_S: arbiter_out port map (X_N_Y => X_N_S, 
-                               X_E_Y => X_E_S , 
-                               X_W_Y => X_W_S, 
-                               X_S_Y => X_S_S, 
-                               X_L_Y => X_L_S, 
+arb_X_S: arbiter_out port map (X_N_Y => X_N_S, X_E_Y => X_E_S, X_W_Y => X_W_S, X_S_Y => X_S_S, X_L_Y => X_L_S, 
                                credit => credit_counter_S_out,
                                grant_Y_N => grant_S_N_sig, 
                                grant_Y_E => grant_S_E_sig, 
@@ -270,11 +198,7 @@ arb_X_S: arbiter_out port map (X_N_Y => X_N_S,
                                grant_Y_L => grant_S_L_sig);
 
 -- Y is L now
-arb_X_L: arbiter_out port map (X_N_Y => X_N_L, 
-                               X_E_Y => X_E_L , 
-                               X_W_Y => X_W_L, 
-                               X_S_Y => X_S_L, 
-                               X_L_Y => X_L_L, 
+arb_X_L: arbiter_out port map (X_N_Y => X_N_L, X_E_Y => X_E_L, X_W_Y => X_W_L, X_S_Y => X_S_L, X_L_Y => X_L_L, 
                                credit => credit_counter_L_out,
                                grant_Y_N => grant_L_N_sig, 
                                grant_Y_E => grant_L_E_sig, 
