9c0dd50693ea139b94e89518f6d4d1557e768c75
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2016-09-01 05:46:13 +0900

    more modular script for network gen CB

---
+++ scripts/Credit_Based/ACII_art.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2016 Siavoosh Payandeh Azad
+
+
+def generate_ascii_art(noc_file, network_dime):
+
+    noc_file.write("\n\n")
+    noc_file.write("--        organizaiton of the network:\n")
+    noc_file.write("--     x --------------->\n")
+    for j in range(0, network_dime):
+        if j == 0:
+          noc_file.write("--  y  ")
+        else:
+          noc_file.write("--  |  ")
+        for i in range(0, network_dime):
+          noc_file.write("       ----")
+        noc_file.write("\n")
+        noc_file.write("--  |       ")
+        for i in range(0, network_dime):
+          if i != network_dime-1:
+            if (i+network_dime*j)>=10:
+              noc_file.write(" | "+str(i+network_dime*j)+" | ---")
+            else:
+              noc_file.write(" | "+str(i+network_dime*j)+"  | ---")
+          else:
+            if (i+network_dime*j)>=10:
+              noc_file.write(" | "+str(i+network_dime*j)+" |")
+            else:
+              noc_file.write(" | "+str(i+network_dime*j)+"  |")
+
+        noc_file.write("\n")
+        if j == network_dime-1:
+          noc_file.write("--  v  ")
+        else:
+          noc_file.write("--  |  ")
+        for i in range(0, network_dime):
+            noc_file.write("       ----")
+
+        if j == network_dime-1:
+          noc_file.write("\n--   ")
+          for i in range(0, network_dime):
+            noc_file.write("           ")
+        else:
+          noc_file.write("\n--  |")
+          for i in range(0, network_dime):
+            noc_file.write("          |")
+
+        noc_file.write("\n")
