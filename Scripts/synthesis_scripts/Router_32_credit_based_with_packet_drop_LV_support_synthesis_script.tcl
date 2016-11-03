5a97cfc953d02de42be5127203b06e62ba0c4fd4
Behrad Niazmand <bniazmand@pralinee.pld.ttu.ee>
2016-11-03 11:19:02 +0200

    Router synthesis script update (counter depth fixed)

--- Scripts/synthesis_scripts/Router_32_credit_based_with_packet_drop_LV_support_synthesis_script.tcl
+++ Scripts/synthesis_scripts/Router_32_credit_based_with_packet_drop_LV_support_synthesis_script.tcl
@@ -12,8 +12,21 @@ analyze -library WORK -format vhdl {../RTL/Fault_Management/Fault_management_net
 
 ####################################################################################################
 
+# FIFO credit-based with packet dropping and fault classification support
+elaborate FIFO_credit_based -architecture behavior -library DEFAULT -parameters "DATA_WIDTH = 32"
+# clock period set to 10 ns
+create_clock -name "clk" -period 10 -waveform { 0 5  }  { clk  }
+# reset signal
+set_dont_touch reset
+# compile 
+compile -exact_map
+# report area (save to output file)
+report_area > area/FIFO_one_hot_credit_based_packet_drop_classifier_support_area.txt
+
+####################################################################################################
+
 # Counter Threshold Classifier
-elaborate counter_threshold_classifier -architecture behavior -library DEFAULT -parameters "counter_depth = 8, healthy_counter_threshold = 4, faulty_counter_threshold = 4"
+elaborate counter_threshold_classifier -architecture behavior -library DEFAULT -parameters "counter_depth = 4, healthy_counter_threshold = 15, faulty_counter_threshold = 4"
 # clock period set to 10 ns
 create_clock -name "clk" -period 10 -waveform { 0 5  }  { clk  }
 # reset signal
@@ -26,7 +39,7 @@ report_area > area/counter_threshold_area.txt
 ####################################################################################################
 
 # Router (credit-based flow control) (32 bit data width, XY routing, router ID 5, network size: 4x4 (2D Mesh))
-elaborate router_credit_based_parity_lv -architecture behavior -library DEFAULT -parameters "DATA_WIDTH = 32, DATA_WIDTH_LV = 11, current_address = 5, Rxy_rst = 60, Cx_rst = 15, healthy_counter_threshold = 8, faulty_counter_threshold = 4, counter_depth = 4, NoC_size = 4"
+elaborate router_credit_based_parity_lv -architecture behavior -library DEFAULT -parameters "DATA_WIDTH = 32, DATA_WIDTH_LV = 11, current_address = 5, Rxy_rst = 60, Cx_rst = 15, healthy_counter_threshold = 15, faulty_counter_threshold = 4, counter_depth = 4, NoC_size = 4"
 # clock period set to 10 ns
 create_clock -name "clk" -period 10 -waveform { 0 5  }  { clk  }
 # reset signal
