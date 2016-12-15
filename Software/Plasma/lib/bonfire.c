8e25b449bc691e7bdbc4c7a30bef76a97c782129
Karl Janson <karl.janson@ati.ttu.ee>
2016-12-15 19:26:21 +0200

    New API. It abstracts out all threading and communications

---
+++ Software/Plasma/lib/bonfire.c
@@ -0,0 +1,109 @@
+#include "bonfire.h"
+#include "plasma.h"
+#include "ni.h"
+#include "packets.h"
+
+/****** Recieve Buffer Initialization ******/
+int recv_pointer = 0;
+int send_pointer = 0;
+
+/********* Flags for Protothreads *********/
+int send_thread_flag    = THREAD_ENABLE;
+int recv_thread_flag    = THREAD_DISABLE;
+int process_thread_flag = THREAD_DISABLE;
+
+/**************** Functions ****************/
+
+/**
+ * Puts a flit to the  send queue.
+ * @param  data The flit to be sent
+ * @return      Error code
+ */
+int bonfire_send(unsigned int data)
+{
+    if (send_pointer < SEND_BUF_SIZE - 1)
+    {
+        send_buffer[send_pointer] = build_header(1,3);
+        send_pointer++;
+
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+/**************** Threads ****************/
+
+/**
+ * NI sending protothread
+ */
+int send_thread(struct pt *pt)
+{
+    int i;
+    PT_BEGIN(pt);
+
+    while(1)
+    {
+        /* Wait for our turn */
+        PT_WAIT_UNTIL(pt, send_thread_flag == THREAD_ENABLE);
+
+        /****** Sending code ******/
+        if (((ni_read_flags() & NI_WRITE_MASK) == 0) && \
+            (send_pointer > 0))
+        {
+            for (i = 0; i < send_pointer; i++)
+            {
+                ni_write(send_buffer[i]);
+            }
+            send_pointer = 0;
+        }
+
+        /****** End of sending code ******/
+
+        /* Next thread's turn */
+        send_thread_flag    = THREAD_DISABLE;
+        recv_thread_flag    = THREAD_ENABLE;
+        process_thread_flag = THREAD_DISABLE;
+    }
+
+    PT_END(pt);
+}
+
+/**
+ * NI receiving protothread
+ */
+int recv_thread(struct pt *pt)
+{
+    unsigned int ni_flags;
+    unsigned int packet_counter = 0;
+
+    PT_BEGIN(pt);
+
+    while(1)
+    {
+        /* Wait for our turn */
+        PT_WAIT_UNTIL(pt, recv_thread_flag == THREAD_ENABLE);
+
+        /****** Recieving code ******/
+        packet_counter = memory_read(NI_COUNTER_ADDRESS);
+        ni_flags = ni_read_flags();
+
+        if ((packet_counter > 0) && \
+            ((ni_flags & NI_READ_MASK) == 0) && \
+            (recv_pointer < RECV_BUF_SIZE - 1))
+        {
+            recv_buffer[recv_pointer] = ni_read();;
+            recv_pointer++;
+        }
+
+        /****** End of recieving code ******/
+
+        /* Next thread's turn */
+        send_thread_flag    = THREAD_DISABLE;
+        recv_thread_flag    = THREAD_DISABLE;
+        process_thread_flag = THREAD_ENABLE;
+    }
+    PT_END(pt);
+}
--- Software/Plasma/lib/bonfire.h
+++ Software/Plasma/lib/bonfire.h
@@ -12,46 +12,91 @@
 #ifndef __BONFIRE_H__
 #define __BONFIRE_H__
 
-#include "../../lib/pt-1.4/pt.h"
+#include "pt-1.4/pt.h"
 
-/* Flags for protothreads */
-static int send_thread_flag, recv_thread_flag;
+/************** Macro Defines **************/
+#define RECV_BUF_SIZE   16
+#define SEND_BUF_SIZE   16
 
-/* Protothread state variables */
-static struct pt pt_send, pt_recv;
+#define THREAD_ENABLE   1
+#define THREAD_DISABLE  0
 
+/*** Recieve Buffers and Their Pointers ***/
+extern int recv_pointer;
+extern int send_pointer;
 
+unsigned int recv_buffer[RECV_BUF_SIZE];
+unsigned int send_buffer[SEND_BUF_SIZE];
+
+/********* Flags for Protothreads *********/
+extern int send_thread_flag;
+extern int recv_thread_flag;
+extern int process_thread_flag;
+
+/****** Protothread State Variables *******/
+struct pt pt_send;
+struct pt pt_recv;
+struct pt pt_process;
+
+/**
+ * Puts a flit to the  send queue.
+ * @param  data The flit to be sent
+ * @return      Error code
+ */
+int bonfire_send(unsigned int data);
 
 /**
  * NI sending protothread
  */
-static int send_thread(struct pt *pt);
+int send_thread(struct pt *pt);
 
 /**
  * NI receiving protothread
  */
-static int recv_thread(struct pt *pt);
+int recv_thread(struct pt *pt);
 
 /**
- * NI data processing protothread
+ * Data processing protothread
  */
-static int data_thread(struct pt *pt);
+int proc_thread(struct pt *pt);
 
-#define BONFIRE_MAIN()                              \
-                                                    \
-    int main() {                                    \
-                                                    \
-        PT_INIT(&pt_send);                          \
-        PT_INIT(&pt_recv);                          \
-                                                    \
-        while (1)                                   \
-        {                                           \
-            send_thread(&pt_send);                  \
-            recv_thread(&pt_recv);                  \
-        }                                           \
-                                                    \
-        return 0;                                   \
-    }                                               \
-    static int data_thread(struct pt *pt)
+
+#define BONFIRE_MAIN()                                                      \
+                                                                            \
+    int main() {                                                            \
+                                                                            \
+        /* Initialize protothreads */                                       \
+        PT_INIT(&pt_send);                                                  \
+        PT_INIT(&pt_recv);                                                  \
+        PT_INIT(&pt_process);                                               \
+                                                                            \
+        while (1)                                                           \
+        {                                                                   \
+            send_thread(&pt_send);                                          \
+            recv_thread(&pt_recv);                                          \
+            proc_thread(&pt_process);                                       \
+        }                                                                   \
+                                                                            \
+        return 0;                                                           \
+    }                                                                       \
+                                                                            \
+    int proc_thread(struct pt *pt)
+
+#define BONFIRE_LOOP()                                                                                                                        \
+    PT_BEGIN(pt);                                                           \
+                                                                            \
+    while(1)                                                                \
+    {                                                                       \
+        /* Wait for our turn */                                             \
+        PT_WAIT_UNTIL(pt, process_thread_flag == THREAD_ENABLE);            \
+        if(1)                                                               \
+
+#define BONFIRE_RET()                                                  \
+        /* Next thread's turn */                                            \
+        send_thread_flag    = THREAD_ENABLE;                                \
+        recv_thread_flag    = THREAD_DISABLE;                               \
+        process_thread_flag = THREAD_DISABLE;                               \
+    }                                                                       \
+    PT_END(pt)
 
 #endif //__NI_H__
