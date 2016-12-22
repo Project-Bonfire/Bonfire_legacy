1f04ae664fed810edd0c7ba6663bf5fadd96582d
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2016-12-22 12:04:40 +0200

    made a 2 port LV network

---
+++ RTL/Fault_Management/Fault_management_network/LW_2_port/LBDR_LV.vhd
@@ -0,0 +1,85 @@
+--Copyright (C) 2016 Siavoosh Payandeh Azad Behrad Niazmand
+
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.all;
+use IEEE.MATH_REAL.ALL;
+
+entity LBDR_LV is
+    generic (
+        cur_addr_rst: integer := 8;
+        Rxy_rst: integer := 8;
+        Cx_rst: integer := 8;
+        NoC_size: integer := 4
+    );
+    port (  reset: in  std_logic;
+            clk: in  std_logic;
+            empty: in  std_logic;
+            dst_addr: in std_logic_vector(NoC_size-1 downto 0);
+            packet_info: in std_logic;
+            flit_type: in std_logic_vector(2 downto 0);
+	          grant_E, grant_W, grant_L: in std_logic;
+            Req_E, Req_W, Req_L:out std_logic
+            );
+end LBDR_LV;
+
+architecture behavior of LBDR_LV is
+
+  signal Cx:  std_logic_vector(3 downto 0);
+  signal Rxy:  std_logic_vector(7 downto 0);
+  signal cur_addr:  std_logic_vector(NoC_size-1 downto 0);  
+  signal N1, E1, W1, S1  :std_logic :='0';  
+  signal grants: std_logic;
+begin 
+
+ grants <= grant_E or grant_W or grant_L;
+
+  Cx       <=  std_logic_vector(to_unsigned(Cx_rst, Cx'length));
+  Rxy      <= std_logic_vector(to_unsigned(Rxy_rst, Rxy'length));
+  cur_addr <= std_logic_vector(to_unsigned(cur_addr_rst, cur_addr'length));
+
+  N1 <= '1' when  dst_addr(NoC_size-1 downto NoC_size/2) < cur_addr(NoC_size-1 downto NoC_size/2) else '0';
+  E1 <= '1' when  cur_addr((NoC_size/2)-1 downto 0) < dst_addr((NoC_size/2)-1 downto 0) else '0';
+  W1 <= '1' when  dst_addr((NoC_size/2)-1 downto 0) < cur_addr((NoC_size/2)-1 downto 0) else '0';
+  S1 <= '1' when  cur_addr(NoC_size-1 downto NoC_size/2) < dst_addr(NoC_size-1 downto NoC_size/2) else '0';
+
+
+process(clk, reset)
+begin
+if reset = '0' then 
+  Req_E_FF <= '0';
+  Req_W_FF <= '0';
+  Req_L_FF <= '0';
+elsif clk'event and clk = '1' then 
+  Req_E_FF <= Req_E_in;
+  Req_W_FF <= Req_W_in;
+  Req_L_FF <= Req_L_in;
+end if;
+end process;
+ 
+
+-- The combionational part
+
+Req_E <= Req_E_FF;
+Req_W <= Req_W_FF;
+Req_L <= Req_L_FF;
+
+process(N1, E1, W1, S1, Rxy, Cx, empty, Req_E_FF, Req_W_FF, Req_L_FF, grants) begin
+ if  flit_type = "001" and empty = '0' then
+        Req_E_in <= ((E1 and not N1 and not S1) or (E1 and N1 and Rxy(2)) or (E1 and S1 and Rxy(3))) and Cx(1);
+        Req_W_in <= ((W1 and not N1 and not S1) or (W1 and N1 and Rxy(4)) or (W1 and S1 and Rxy(5))) and Cx(2);
+        Req_L_in <= not N1 and  not E1 and not W1 and not S1;
+  elsif  ( flit_type = "100" and empty = '0' and grants = '1') then
+        Req_E_in <= '0';
+        Req_W_in <= '0';
+        Req_L_in <= '0';
+  else
+        Req_E_in <= Req_E_FF;
+        Req_W_in <= Req_W_FF;
+        Req_L_in <= Req_L_FF;
+  end if;
+end process;
+   
+END;
---
+++ RTL/Fault_Management/Fault_management_network/LW_2_port/arbiter_out.vhd
@@ -0,0 +1,87 @@
+--Copyright (C) 2016 Siavoosh Payandeh Azad
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity arbiter_out_LV is
+    port (  
+            reset: in  std_logic;
+            clk: in  std_logic;
+            X_E_Y, X_W_Y, X_L_Y :in std_logic; -- From LBDR modules
+            credit: in std_logic;
+            grant_Y_E, grant_Y_W, grant_Y_L : out std_logic -- Grants given to LBDR requests (encoded as one-hot)
+            );
+end;
+
+architecture behavior of arbiter_out_LV is
+  TYPE STATE_TYPE IS (IDLE, East, West, Local);
+  SIGNAL state, state_in   : STATE_TYPE := IDLE;
+
+begin
+process (clk, reset)begin
+  if reset = '0' then
+      state <= IDLE;
+  elsif clk'event and clk ='1'then
+      state <= state_in;
+  end if;
+end process;
+
+-- anything below here is pure combinational
+process(state, X_E_Y, X_W_Y, X_L_Y, credit) begin
+    grant_Y_E <= '0';
+    grant_Y_W <= '0';
+    grant_Y_L <= '0';
+    case state is 
+      when IDLE =>
+          if X_E_Y = '1' then
+              state_in <= East;
+          elsif X_W_Y = '1' then
+              state_in <= West;
+          elsif X_L_Y = '1' then
+              state_in <= Local;
+          else
+              state_in <= IDLE;
+          end if; 
+      when East =>
+          if credit = '1' then
+            grant_Y_E <= '1';
+          end if;
+          if X_E_Y = '1' then
+              state_in <= East;
+          elsif X_W_Y = '1' then
+              state_in <= West;
+          elsif X_L_Y = '1' then
+              state_in <= Local;
+          else
+              state_in <= IDLE;
+          end if;
+      when West =>
+          if credit = '1' then
+            grant_Y_W <= '1';
+          end if;
+          if X_W_Y = '1' then
+              state_in <= West;
+          elsif X_L_Y = '1' then
+              state_in <= Local;
+          elsif X_E_Y = '1' then
+              state_in <= East;
+          else
+              state_in <= IDLE;
+          end if;
+      when others =>
+          if credit = '1' then
+            grant_Y_L <= '1';
+          end if;
+          if X_L_Y = '1' then
+              state_in <= Local;
+          elsif X_E_Y = '1' then
+              state_in <= East;
+          elsif X_W_Y = '1' then
+              state_in <= West;
+          else
+              state_in <= IDLE;
+          end if; 
+    end case;
+
+end process;
+end;
