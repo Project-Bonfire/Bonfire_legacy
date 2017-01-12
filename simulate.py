4b61983638caab3e077743fa29c38b6369dad45b
Siavoosh.Azad <siavoosh.azad@ttu.ee>
2017-01-12 14:07:55 +0200

    added preview to sim script

--- simulate.py
+++ simulate.py
@@ -15,7 +15,7 @@ from Scripts.include.write_do_file import write_do_file
 from Scripts.include import package
 from Scripts.include.arg_parser import arg_parser, report_parogram_arguments
 from Scripts.include.stats import statistics
-# from Scripts.include.viz_traffic import viz_traffic
+from Scripts.include.viz_traffic import viz_traffic
 from Scripts.include.file_gen import gen_network_and_tb, gen_wave_do
 
 """
@@ -113,8 +113,8 @@ def main(argv):
     else:
         statistics(False)
 
-    #if package.program_argv["trace"]:
-    #    viz_traffic(package.program_argv["network_dime"])
+    if package.program_argv["trace"]:
+        viz_traffic(package.program_argv["network_dime"])
 
     logging.info('Logging finished...')
 
