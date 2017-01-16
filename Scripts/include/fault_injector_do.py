50aea460a3d2149a9a9c1cd6388611bdc678bc88
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2017-01-16 22:41:22 +0200

    can inject faults with any width!

--- Scripts/include/fault_injector_do.py
+++ Scripts/include/fault_injector_do.py
@@ -2,18 +2,15 @@ import random
 
 FPS = 10000000
 Sim_time = 10000
+links = {"RX_L_2": 32,  "RX_L_3": 32, "RX_L_1": 1}
 
-
-bit_width = 32
-list_of_links = ["RX_L_2", "RX_L_3","RX_L_1"]
-
-def generate_fault_injection_do(bit_width, sim_time, FPS, list_of_links):
+def generate_fault_injection_do(sim_time, FPS, links):
 	"""
-	bit_width: integer : bit width of the signals in list_of_links
 	sim_time: integer : How long do you want to run the simulation in ns
 	FPS: integer : faults per second
 	list_of_links: list: list of links for injection
 	"""
+	list_of_links = links.keys()
 	delay = 1000000000/FPS
 	deviation = int(delay/100)
 	fault_inject_file = open('fault_inject.do', 'w')
@@ -21,11 +18,12 @@ def generate_fault_injection_do(bit_width, sim_time, FPS, list_of_links):
 		random.shuffle(list_of_links) 
 		sum_skew = 0
 		for item in list_of_links:
+			bit_width = links[item]
 			random_position = random.randint(0, bit_width-1)
 			string = "force -drive sim/:tb_network_2x2:"+item+" "
 			for j in range(0, random_position):
 				string += "0"
-			string +=  "1"
+			string +=  str(random.choice(["0", "1"]))
 			for j in range(random_position+1, bit_width):
 				string +=  "0"
 			string +=  " 0 -cancel 1ns"
@@ -39,4 +37,4 @@ def generate_fault_injection_do(bit_width, sim_time, FPS, list_of_links):
 	fault_inject_file.close()
 
 
-generate_fault_injection_do(bit_width, Sim_time, FPS, list_of_links)
+generate_fault_injection_do(Sim_time, FPS, links)
