aafaab9ccccc151bf3520778bd7e347171a1c1af
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2017-01-21 12:02:17 +0200

    Added Transient to Intermittent, single Permanent and also documentation
    Preparing to add the signals for each module in there!

--- Scripts/include/fault_injector_do.py
+++ Scripts/include/fault_injector_do.py
@@ -24,7 +24,7 @@ class fault:
 		"""
 		The fault reports its location, signal width, type, MTBF, STD_Dev and shutdown time!
 		"""
-		print "Location: ", self.location, "\twidth: ", self.bitwidth,  "\tfault_type: ", self.Type,\
+		print "Location: ", self.location, "\twidth: ", self.bitwidth,  "\tfault_type: ", '%5s' %self.Type,\
 			  "\tMTBF: ", self.mean_time, "\tstd deviation: ", self.std_dev , "\tshutdown time", \
 			  self.shut_down_time
 
@@ -38,6 +38,24 @@ def report_faults(fault_list):
 		fault.report()
 	print "---------------------------------------"
 
+#----------------------------------------------------------------------------------------------
+# Generating signals for different modules                           
+# for this purpose we only consider fault injection points marked with X:                              
+#	
+#                       .-------------.
+#                .----> |  Checkers   | <---.
+#                |      |   Module    |     |
+#                |      '-------------'     |
+#                |             ^            |
+#                |             |            |
+#                |             X            |
+#                |      .-------------.     |
+#                |      |   Module    |     |
+#           -----o--X-->|    under    |--X--o------->
+#                       |    check    |
+#                       '-------------'
+#
+#----------------------------------------------------------------------------------------------
 def list_all_the_links(network_size):
 	"""
 	takes the network size and returns a list of all the RX signals in the network
@@ -55,6 +73,33 @@ def list_all_the_links(network_size):
 			list_of_ports.append("tb_network_"+str(network_size)+"x"+str(network_size)+":NoC:R_"+str(i)+":RX_W")
 	return list_of_ports
 
+def list_all_the_lbdr_signals(network_size):
+	"""
+	takes the network size and returns a list of all the relevant LBDR signals in the network
+	"""
+	list_of_ports = []
+	for i in range(0, network_size*2):
+		pass
+	return list_of_ports
+
+def list_all_the_arbiter_signals(network_size):
+	"""
+	takes the network size and returns a list of all the relevant arbiter signals in the network
+	"""
+	list_of_ports = []
+	for i in range(0, network_size*2):
+		pass
+	return list_of_ports
+
+def list_all_the_fifo_signals(network_size):
+	"""
+	takes the network size and returns a list of all the relevant FIFO signals in the network
+	"""
+	list_of_ports = []
+	for i in range(0, network_size*2):
+		pass
+	return list_of_ports
+
 def generate_links_dictionary(network_size, sim_time):
 	"""
 	This function generates random faults on all RX signals of the network
@@ -63,9 +108,10 @@ def generate_links_dictionary(network_size, sim_time):
 	random.seed(FAULT_RANDOM_SEED)
 	fault_list = []
 	for item in list_of_ports:
-		fault_type = random.choice(["T", "P", "I"])
+		fault_type = random.choice(["T", "P", "I", "T->P", "T->I"])
 		shut_down_time = None
-		if fault_type == "T":
+		std_dev = None
+		if fault_type == "T":		# Transient fault
 			frequency = random.choice(["H", "M", "L"])
 			if frequency == "H":
 				mean_time = int((1000000000/Fault_Per_Second)/HIGH_FAULT_RATE)
@@ -73,13 +119,23 @@ def generate_links_dictionary(network_size, sim_time):
 				mean_time = int((1000000000/Fault_Per_Second)/LOW_FAULT_RATE)
 			else:
 				mean_time = int((1000000000/Fault_Per_Second)/MEDIUM_FAULT_RATE)
-		elif fault_type == "I":
+			std_dev = int(mean_time*0.1+1)
+
+		elif fault_type == "I" or fault_type == "T->I": 	# Intermittent fault or transient to intermittent 
 			mean_time = int(MTB_INTERMITTENT_BURST)
-		else:	# its Permentent fault
-			mean_time = int(1000000000/Fault_Per_Second)
+			std_dev = int(mean_time*0.1+1)
+
+		elif fault_type == "P":		# its Permentent fault
+			mean_time = None
+			std_dev = None
 			shut_down_time = random.randint(int(sim_time*0.1), int(sim_time*0.9))
 
-		new_fault = fault(item, 32, fault_type, mean_time, int(mean_time*0.1+1), shut_down_time)
+		elif fault_type == "T->P":	# Transient goes to Intermittent and then to Permanent
+			mean_time = int(1000000000/Fault_Per_Second)
+			shut_down_time = random.randint(int(sim_time*0.1), int(sim_time*0.9))
+			std_dev = int(mean_time*0.1+1)
+
+		new_fault = fault(item, 32, fault_type, mean_time, std_dev, shut_down_time)
 		fault_list.append(new_fault)
 
 	report_faults(fault_list)
@@ -97,7 +153,9 @@ def parse_fault_info_file(file_path):
 	fault_type: 	should be chosen from the follwoing list:
 							* T : Transient
 							* I : Intermittent
-							* F : Faulty
+							* P : Permanent
+							* T->P : Transient to Intermittent to permanent
+							* T->I : Transient to Intermittent
 	MTBF: 			Mean time between the faults
 	std_deviation:	Standard deviation used for generating faults
 	shutdown_time:	Time in ns when the signal would be permanently faulty only used when 
@@ -154,6 +212,7 @@ def generate_fault_injection_do(file_path, sim_time, sim_end, fault_list):
 					temp_dict[int(fault_time)] = [item]
 				time_until_next_fault = numpy.random.normal(item.mean_time, item.std_dev)
 				fault_time += time_until_next_fault
+		
 		if item.Type == "I": 
 			fault_time = 0
 			time_until_next_fault = numpy.random.normal(item.mean_time, item.std_dev)
@@ -167,7 +226,36 @@ def generate_fault_injection_do(file_path, sim_time, sim_end, fault_list):
 				time_until_next_fault = numpy.random.normal(item.mean_time, item.std_dev)
 				fault_time += time_until_next_fault
 
+		if item.Type == "T->I":
+			permanently_faulty_locations.append(item)
+			fault_time = 0
+			time_until_next_fault = numpy.random.normal(item.mean_time, item.std_dev)
+			fault_time += time_until_next_fault
+			while fault_time < int(sim_time*0.5):
+				if int(fault_time) in temp_dict.keys():
+					temp_dict[int(fault_time)].append(item)
+				else:
+					temp_dict[int(fault_time)] = [item]
+				time_until_next_fault = numpy.random.normal(item.mean_time, item.std_dev)
+				fault_time += time_until_next_fault
+
+			time_until_next_fault = numpy.random.normal(int(MTB_INTERMITTENT_BURST), \
+														int(MTB_INTERMITTENT_BURST*0.1+1))
+			fault_time += time_until_next_fault
+			while fault_time+EVENTS_PER_BURST < int(sim_time):
+				for event in range(0, EVENTS_PER_BURST):
+					if int(fault_time+event) in temp_dict.keys():
+						temp_dict[int(fault_time+event)].append(item)
+					else:
+						temp_dict[int(fault_time+event)] = [item]
+				time_until_next_fault = numpy.random.normal(int(MTB_INTERMITTENT_BURST), \
+															int(MTB_INTERMITTENT_BURST*0.1+1))
+				fault_time += time_until_next_fault
+
 		if item.Type == "P":
+			permanently_faulty_locations.append(item)
+
+		if item.Type == "T->P":
 			permanently_faulty_locations.append(item)
 			fault_time = 0
 			time_until_next_fault = numpy.random.normal(item.mean_time, item.std_dev)
@@ -199,14 +287,17 @@ def generate_fault_injection_do(file_path, sim_time, sim_end, fault_list):
 		for permanent_fault_location in permanently_faulty_locations:
 			if i == permanent_fault_location.shut_down_time:
 				location  = permanent_fault_location.location
+				fault_inject_file.write("# ###################################################\n")
+				fault_inject_file.write("# Shutting down signal: "+location+" for good!\n")
 				fault_inject_file.write("force -drive sim/:"+location+" U 1ns\n")
+				fault_inject_file.write("# ###################################################\n")
 		if i in temp_dict.keys():
 			last_time = current_time
 			current_time = i
 			fault_inject_file.write("run "+str(current_time-last_time)+"ns\n") 
 			for item in temp_dict[i]:
 				location = item.location
-				if item.Type == "I" or item.Type == "P":
+				if item.Type == "I" or item.Type == "T->I" or item.Type == "T->P":
 					string = "force -drive sim/:"+location+" " + str(random.choice(["0", "1"])) 
 					string +=  " 0 ns -cancel 1ns"
 				else:
