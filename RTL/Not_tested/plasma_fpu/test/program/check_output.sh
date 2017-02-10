630d801f6146102ca4fc5de3e43c722ef5becb5b
Karl Janson <karl.janson@ati.ttu.ee>
2017-02-10 15:39:39 +0200

    Removed Plasma with FPU, since it is not synthesizable

--- RTL/Not_tested/plasma_fpu/test/program/check_output.sh
+++
@@ -1,9 +0,0 @@
-# <Feb. 7th 2017> edit by Behrad Niazmand (bniazmand@ati.ttu.ee), added "perl" to the beginning of the files to use perl when executing the script.
-
-#!/bin/bash
-
-# convert from memory to txt file
-perl file2out.pl -txt memory_out.mem > memory_out.txt
-
-# create binary file
-perl file2out.pl -bin memory_out.txt test_out.jp2
--- RTL/Not_tested/plasma_fpu/test/scripts/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# uncomment this line for command line simulation only
-# vsim -c -do "source run.tcl"
-
-# simulation with GUI
-vsim -do "source run.tcl"
