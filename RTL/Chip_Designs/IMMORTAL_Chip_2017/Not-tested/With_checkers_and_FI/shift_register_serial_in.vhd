5698c241626f3106ff80d9d89d01b017a57ca190
Siavoosh.Azad <siavoosh.azad@ttu.ee>
2017-04-03 11:08:22 +0300

    updated shift register for IJTAG

--- RTL/Chip_Designs/IMMORTAL_Chip_2017/Not-tested/With_checkers_and_FI/shift_register_serial_in.vhd
+++ RTL/Chip_Designs/IMMORTAL_Chip_2017/Not-tested/With_checkers_and_FI/shift_register_serial_in.vhd
@@ -8,32 +8,39 @@ entity shift_register_serial_in is
         REG_WIDTH: integer := 32
     );
     port (
-        clk, reset : in std_logic;
-        shift: in std_logic;
-        data_in_serial: in std_logic;
-        data_out_parallel: out std_logic_vector(REG_WIDTH-1 downto 0);
-        data_out_serial: out std_logic
+        TCK, reset : in std_logic;
+        SE: in std_logic;
+        UE: in std_logic;
+        SI: in std_logic;
+        SO: out std_logic;
+        data_out_parallel: out std_logic_vector(REG_WIDTH-1 downto 0)
     );
 end;
 
 architecture behavior of shift_register_serial_in is
-
 signal shift_register_mem_out : std_logic_vector(REG_WIDTH-1 downto 0); 
 
 begin
-process (clk, reset)
+process (TCK, reset)
 begin 
     if reset = '0'  then
         shift_register_mem_out <= (others => '0');
-    elsif clk'event and clk = '1' then 
-        if shift = '1' then
-            shift_register_mem_out <=  shift_register_mem_out (REG_WIDTH-2 downto 0) & data_in_serial;
+    elsif TCK'event and TCK = '1' then 
+        if SE = '1' then
+            shift_register_mem_out <=  shift_register_mem_out (REG_WIDTH-2 downto 0) & SI;
         end if;
     end if;
 
 end process;
 
-data_out_parallel <= shift_register_mem_out;
-data_out_serial <= shift_register_mem_out (REG_WIDTH-2);
+process(UE, shift_register_mem_out) begin
+    if UE = '1'  then
+        data_out_parallel <= shift_register_mem_out;
+    else
+        data_out_parallel <= (others => '0');
+    end if;
+end process;
+
+SO <= shift_register_mem_out (REG_WIDTH-1);
 
 end;
