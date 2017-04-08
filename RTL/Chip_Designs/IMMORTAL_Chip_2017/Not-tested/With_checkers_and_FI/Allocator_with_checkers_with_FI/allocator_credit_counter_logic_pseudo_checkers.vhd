c4c65a8a86d1388fb9dadbcd4be86376b2d121ca
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2017-04-08 14:02:02 +0300

    checker code cleanup

--- RTL/Chip_Designs/IMMORTAL_Chip_2017/Not-tested/With_checkers_and_FI/Allocator_with_checkers_with_FI/allocator_credit_counter_logic_pseudo_checkers.vhd
+++ RTL/Chip_Designs/IMMORTAL_Chip_2017/Not-tested/With_checkers_and_FI/Allocator_with_checkers_with_FI/allocator_credit_counter_logic_pseudo_checkers.vhd
@@ -65,55 +65,49 @@ begin
 
 process (credit_in_N, valid_N, credit_counter_N_in, credit_counter_N_out)
 begin
+  err_credit_in_N_grant_N_credit_counter_N_in_credit_counter_N_out_equal <= '0';
   if (credit_in_N = '1' and valid_N = '1' and credit_counter_N_in /= credit_counter_N_out) then
       err_credit_in_N_grant_N_credit_counter_N_in_credit_counter_N_out_equal <= '1';
-  else
-      err_credit_in_N_grant_N_credit_counter_N_in_credit_counter_N_out_equal <= '0';
   end if;
 end process;
 
 process (credit_in_N, valid_N, credit_counter_N_in, credit_counter_N_out)
 begin
+  err_credit_in_N_credit_counter_N_out_increment <= '0';
   if (credit_in_N = '1' and valid_N = '0' and credit_counter_N_out < 3 and credit_counter_N_in /= credit_counter_N_out + 1) then
       err_credit_in_N_credit_counter_N_out_increment <= '1';
-  else
-      err_credit_in_N_credit_counter_N_out_increment <= '0';
   end if;
 end process;
 
 process (credit_in_N, valid_N, credit_counter_N_in, credit_counter_N_out)
 begin
+  err_not_credit_in_N_credit_counter_N_out_max_credit_counter_N_in_not_change <= '0';
   if (credit_in_N = '1' and valid_N = '0' and credit_counter_N_out = 3 and credit_counter_N_in /= credit_counter_N_out) then
       err_not_credit_in_N_credit_counter_N_out_max_credit_counter_N_in_not_change <= '1';
-  else
-      err_not_credit_in_N_credit_counter_N_out_max_credit_counter_N_in_not_change <= '0';
   end if;
 end process;
 
 process (valid_N, credit_in_N, credit_counter_N_in, credit_counter_N_out)
 begin
+  err_grant_N_credit_counter_N_out_decrement <= '0';
   if (valid_N = '1' and credit_in_N = '0' and credit_counter_N_out > 0 and credit_counter_N_in /= credit_counter_N_out - 1) then
       err_grant_N_credit_counter_N_out_decrement <= '1';
-  else
-      err_grant_N_credit_counter_N_out_decrement <= '0';
   end if;
 end process;
 
 process (valid_N, credit_in_N, credit_counter_N_in, credit_counter_N_out)
 begin
+  err_not_grant_N_or_credit_counter_N_out_zero_credit_counter_N_in_not_change <= '0';
   if (valid_N = '1' and credit_in_N = '0' and credit_counter_N_out = 0 and credit_counter_N_in /= credit_counter_N_out) then
       err_not_grant_N_or_credit_counter_N_out_zero_credit_counter_N_in_not_change <= '1';
-  else
-      err_not_grant_N_or_credit_counter_N_out_zero_credit_counter_N_in_not_change <= '0';
   end if;
 end process;
 
 process (credit_in_N, valid_N, credit_counter_N_in, credit_counter_N_out)
 begin
+  err_not_credit_in_N_not_grant_N_credit_counter_N_in_credit_counter_N_out_equal <= '0';
   if (credit_in_N = '0' and valid_N = '0' and credit_counter_N_in /= credit_counter_N_out) then
       err_not_credit_in_N_not_grant_N_credit_counter_N_in_credit_counter_N_out_equal <= '1';
-  else
-      err_not_credit_in_N_not_grant_N_credit_counter_N_in_credit_counter_N_out_equal <= '0';
   end if;
 end process;
 
@@ -121,55 +115,49 @@ end process;
 
 process (credit_in_E, valid_E, credit_counter_E_in, credit_counter_E_out)
 begin
+  err_credit_in_E_grant_E_credit_counter_E_in_credit_counter_E_out_equal <= '0';
   if (credit_in_E = '1' and valid_E = '1' and credit_counter_E_in /= credit_counter_E_out) then
-      err_credit_in_E_grant_E_credit_counter_E_in_credit_counter_E_out_equal <= '1';
-  else
-      err_credit_in_E_grant_E_credit_counter_E_in_credit_counter_E_out_equal <= '0';
+    err_credit_in_E_grant_E_credit_counter_E_in_credit_counter_E_out_equal <= '1';
   end if;
 end process;
 
 process (credit_in_E, valid_E, credit_counter_E_in, credit_counter_E_out)
 begin
+  err_credit_in_E_credit_counter_E_out_increment <= '0';
   if (credit_in_E = '1' and valid_E = '0' and credit_counter_E_out < 3 and credit_counter_E_in /= credit_counter_E_out + 1) then
-      err_credit_in_E_credit_counter_E_out_increment <= '1';
-  else
-      err_credit_in_E_credit_counter_E_out_increment <= '0';
+    err_credit_in_E_credit_counter_E_out_increment <= '1';
   end if;
 end process;
 
 process (credit_in_E, valid_E, credit_counter_E_in, credit_counter_E_out)
 begin
+  err_not_credit_in_E_credit_counter_E_out_max_credit_counter_E_in_not_change <= '0';
   if (credit_in_E = '1' and valid_E = '0' and  credit_counter_E_out = 3 and credit_counter_E_in /= credit_counter_E_out) then
-      err_not_credit_in_E_credit_counter_E_out_max_credit_counter_E_in_not_change <= '1';
-  else
-      err_not_credit_in_E_credit_counter_E_out_max_credit_counter_E_in_not_change <= '0';
+    err_not_credit_in_E_credit_counter_E_out_max_credit_counter_E_in_not_change <= '1';
   end if;
 end process;
 
 process (valid_E, credit_in_E, credit_counter_E_in, credit_counter_E_out)
 begin
+  err_grant_E_credit_counter_E_out_decrement <= '0';
   if (valid_E = '1' and credit_in_E = '0' and credit_counter_E_out > 0 and credit_counter_E_in /= credit_counter_E_out - 1) then
-      err_grant_E_credit_counter_E_out_decrement <= '1';
-  else
-      err_grant_E_credit_counter_E_out_decrement <= '0';
+    err_grant_E_credit_counter_E_out_decrement <= '1';
   end if;
 end process;
 
 process (valid_E, credit_in_E, credit_counter_E_in, credit_counter_E_out)
 begin
+  err_not_grant_E_or_credit_counter_E_out_zero_credit_counter_E_in_not_change <= '0';
   if (valid_E = '1' and credit_in_E = '0' and credit_counter_E_out = 0 and credit_counter_E_in /= credit_counter_E_out) then
       err_not_grant_E_or_credit_counter_E_out_zero_credit_counter_E_in_not_change <= '1';
-  else
-      err_not_grant_E_or_credit_counter_E_out_zero_credit_counter_E_in_not_change <= '0';
   end if;
 end process;
 
 process (credit_in_E, valid_E, credit_counter_E_in, credit_counter_E_out)
 begin
+  err_not_credit_in_E_not_grant_E_credit_counter_E_in_credit_counter_E_out_equal <= '0';
   if (credit_in_E = '0' and valid_E = '0' and credit_counter_E_in /= credit_counter_E_out) then
       err_not_credit_in_E_not_grant_E_credit_counter_E_in_credit_counter_E_out_equal <= '1';
-  else
-      err_not_credit_in_E_not_grant_E_credit_counter_E_in_credit_counter_E_out_equal <= '0';
   end if;
 end process;
 
@@ -177,55 +165,49 @@ end process;
 
 process (credit_in_W, valid_W, credit_counter_W_in, credit_counter_W_out)
 begin
+  err_credit_in_W_grant_W_credit_counter_W_in_credit_counter_W_out_equal <= '0';
   if (credit_in_W = '1' and valid_W = '1' and credit_counter_W_in /= credit_counter_W_out) then
       err_credit_in_W_grant_W_credit_counter_W_in_credit_counter_W_out_equal <= '1';
-  else
-      err_credit_in_W_grant_W_credit_counter_W_in_credit_counter_W_out_equal <= '0';
   end if;
 end process;
 
 process (credit_in_W, valid_W, credit_counter_W_in, credit_counter_W_out)
 begin
+  err_credit_in_W_credit_counter_W_out_increment <= '0';
   if (credit_in_W = '1' and valid_W = '0' and credit_counter_W_out < 3 and credit_counter_W_in /= credit_counter_W_out + 1) then
       err_credit_in_W_credit_counter_W_out_increment <= '1';
-  else
-      err_credit_in_W_credit_counter_W_out_increment <= '0';
   end if;
 end process;
 
 process (credit_in_W, valid_W, credit_counter_W_in, credit_counter_W_out)
 begin
+  err_not_credit_in_W_credit_counter_W_out_max_credit_counter_W_in_not_change <= '0';
   if ( (credit_in_W = '1' and valid_W = '0' and credit_counter_W_out = 3) and credit_counter_W_in /= credit_counter_W_out) then
       err_not_credit_in_W_credit_counter_W_out_max_credit_counter_W_in_not_change <= '1';
-  else
-      err_not_credit_in_W_credit_counter_W_out_max_credit_counter_W_in_not_change <= '0';
   end if;
 end process;
 
 process (valid_W, credit_in_W, credit_counter_W_in, credit_counter_W_out)
 begin
+  err_grant_W_credit_counter_W_out_decrement <= '0';
   if (valid_W = '1' and credit_in_W = '0' and credit_counter_W_out > 0 and credit_counter_W_in /= credit_counter_W_out - 1) then
       err_grant_W_credit_counter_W_out_decrement <= '1';
-  else
-      err_grant_W_credit_counter_W_out_decrement <= '0';
   end if;
 end process;
 
 process (valid_W, credit_in_W, credit_counter_W_in, credit_counter_W_out)
 begin
+  err_not_grant_W_or_credit_counter_W_out_zero_credit_counter_W_in_not_change <= '0';
   if ( valid_W = '1' and credit_in_W = '0' and credit_counter_W_out = 0 and credit_counter_W_in /= credit_counter_W_out) then
       err_not_grant_W_or_credit_counter_W_out_zero_credit_counter_W_in_not_change <= '1';
-  else
-      err_not_grant_W_or_credit_counter_W_out_zero_credit_counter_W_in_not_change <= '0';
   end if;
 end process;
 
 process (credit_in_W, valid_W, credit_counter_W_in, credit_counter_W_out)
 begin
+  err_not_credit_in_W_not_grant_W_credit_counter_W_in_credit_counter_W_out_equal <= '0';
   if (credit_in_W = '0' and valid_W = '0' and credit_counter_W_in /= credit_counter_W_out) then
       err_not_credit_in_W_not_grant_W_credit_counter_W_in_credit_counter_W_out_equal <= '1';
-  else
-      err_not_credit_in_W_not_grant_W_credit_counter_W_in_credit_counter_W_out_equal <= '0';
   end if;
 end process;
 
@@ -233,55 +215,49 @@ end process;
 
 process (credit_in_S, valid_S, credit_counter_S_in, credit_counter_S_out)
 begin
+  err_credit_in_S_grant_S_credit_counter_S_in_credit_counter_S_out_equal <= '0';
   if (credit_in_S = '1' and valid_S = '1' and credit_counter_S_in /= credit_counter_S_out) then
       err_credit_in_S_grant_S_credit_counter_S_in_credit_counter_S_out_equal <= '1';
-  else
-      err_credit_in_S_grant_S_credit_counter_S_in_credit_counter_S_out_equal <= '0';
   end if;
 end process;
 
 process (credit_in_S, valid_S, credit_counter_S_in, credit_counter_S_out)
 begin
+  err_credit_in_S_credit_counter_S_out_increment <= '0';
   if (credit_in_S = '1' and valid_S = '0' and credit_counter_S_out < 3 and credit_counter_S_in /= credit_counter_S_out + 1) then
       err_credit_in_S_credit_counter_S_out_increment <= '1';
-  else
-      err_credit_in_S_credit_counter_S_out_increment <= '0';
   end if;
 end process;
 
 process (credit_in_S, valid_S, credit_counter_S_in, credit_counter_S_out)
 begin
+  err_not_credit_in_S_credit_counter_S_out_max_credit_counter_S_in_not_change <= '0';
   if ( credit_in_S = '1' and valid_S = '0' and credit_counter_S_out = 3 and credit_counter_S_in /= credit_counter_S_out) then
       err_not_credit_in_S_credit_counter_S_out_max_credit_counter_S_in_not_change <= '1';
-  else
-      err_not_credit_in_S_credit_counter_S_out_max_credit_counter_S_in_not_change <= '0';
   end if;
 end process;
 
 process (valid_S, credit_in_S, credit_counter_S_in, credit_counter_S_out)
 begin
+  err_grant_S_credit_counter_S_out_decrement <= '0';
   if (valid_S = '1' and credit_in_S = '0' and credit_counter_S_out > 0 and credit_counter_S_in /= credit_counter_S_out - 1) then
       err_grant_S_credit_counter_S_out_decrement <= '1';
-  else
-      err_grant_S_credit_counter_S_out_decrement <= '0';
   end if;
 end process;
 
 process (valid_S, credit_in_S, credit_counter_S_in, credit_counter_S_out)
 begin
+  err_not_grant_S_or_credit_counter_S_out_zero_credit_counter_S_in_not_change <= '0';
   if (valid_S = '1' and credit_in_S = '0' and credit_counter_S_out = 0 and credit_counter_S_in /= credit_counter_S_out) then
       err_not_grant_S_or_credit_counter_S_out_zero_credit_counter_S_in_not_change <= '1';
-  else
-      err_not_grant_S_or_credit_counter_S_out_zero_credit_counter_S_in_not_change <= '0';
   end if;
 end process;
 
 process (credit_in_S, valid_S, credit_counter_S_in, credit_counter_S_out)
 begin
+  err_not_credit_in_S_not_grant_S_credit_counter_S_in_credit_counter_S_out_equal <= '0';
   if (credit_in_S = '0' and valid_S = '0' and credit_counter_S_in /= credit_counter_S_out) then
       err_not_credit_in_S_not_grant_S_credit_counter_S_in_credit_counter_S_out_equal <= '1';
-  else
-      err_not_credit_in_S_not_grant_S_credit_counter_S_in_credit_counter_S_out_equal <= '0';
   end if;
 end process;
 
@@ -289,55 +265,49 @@ end process;
 
 process (credit_in_L, valid_L, credit_counter_L_in, credit_counter_L_out)
 begin
+  err_credit_in_L_grant_L_credit_counter_L_in_credit_counter_L_out_equal <= '0';
   if (credit_in_L = '1' and valid_L = '1' and credit_counter_L_in /= credit_counter_L_out) then
       err_credit_in_L_grant_L_credit_counter_L_in_credit_counter_L_out_equal <= '1';
-  else
-      err_credit_in_L_grant_L_credit_counter_L_in_credit_counter_L_out_equal <= '0';
   end if;
 end process;
 
 process (credit_in_L, valid_L, credit_counter_L_in, credit_counter_L_out)
 begin
+  err_credit_in_L_credit_counter_L_out_increment <= '0';
   if (credit_in_L = '1' and valid_L = '0' and credit_counter_L_out < 3 and credit_counter_L_in /= credit_counter_L_out + 1) then
       err_credit_in_L_credit_counter_L_out_increment <= '1';
-  else
-      err_credit_in_L_credit_counter_L_out_increment <= '0';
   end if;
 end process;
 
 process (credit_in_L, valid_L, credit_counter_L_in, credit_counter_L_out)
 begin
+  err_not_credit_in_L_credit_counter_L_out_max_credit_counter_L_in_not_change <= '0';
   if (credit_in_L = '1' and valid_L = '0' and credit_counter_L_out = 3 and credit_counter_L_in /= credit_counter_L_out) then
       err_not_credit_in_L_credit_counter_L_out_max_credit_counter_L_in_not_change <= '1';
-  else
-      err_not_credit_in_L_credit_counter_L_out_max_credit_counter_L_in_not_change <= '0';
   end if;
 end process;
 
 process (valid_L, credit_in_L, credit_counter_L_in, credit_counter_L_out)
 begin
+  err_grant_L_credit_counter_L_out_decrement <= '0';
   if (valid_L = '1' and credit_in_L = '0' and credit_counter_L_out > 0 and credit_counter_L_in /= credit_counter_L_out - 1) then
       err_grant_L_credit_counter_L_out_decrement <= '1';
-  else
-      err_grant_L_credit_counter_L_out_decrement <= '0';
   end if;
 end process;
 
 process (valid_L, credit_in_L, credit_counter_L_in, credit_counter_L_out)
 begin
+  err_not_grant_L_or_credit_counter_L_out_zero_credit_counter_L_in_not_change <= '0';
   if (valid_L = '1' and credit_in_L = '0' and credit_counter_L_out = 0 and credit_counter_L_in /= credit_counter_L_out) then
       err_not_grant_L_or_credit_counter_L_out_zero_credit_counter_L_in_not_change <= '1';
-  else
-      err_not_grant_L_or_credit_counter_L_out_zero_credit_counter_L_in_not_change <= '0';
   end if;
 end process;
 
 process (credit_in_L, valid_L, credit_counter_L_in, credit_counter_L_out)
 begin
+  err_not_credit_in_L_not_grant_L_credit_counter_L_in_credit_counter_L_out_equal <= '0';
   if (credit_in_L = '0' and valid_L = '0' and credit_counter_L_in /= credit_counter_L_out) then
       err_not_credit_in_L_not_grant_L_credit_counter_L_in_credit_counter_L_out_equal <= '1';
-  else
-      err_not_credit_in_L_not_grant_L_credit_counter_L_in_credit_counter_L_out_equal <= '0';
   end if;
 end process;
 
