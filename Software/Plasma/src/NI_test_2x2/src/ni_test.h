aec6f7969b2f999fab013e7af58a0d877aaa8342
tsotnep@gmail.com <tsotnep@gmail.com>
2017-06-07 18:24:20 +0300

    synthesis enabled

--- Software/Plasma/src/NI_test_2x2/src/ni_test.h
+++ Software/Plasma/src/NI_test_2x2/src/ni_test.h
@@ -1,8 +1,8 @@
-// #define CPU_SPEED       25000000 //Mhz
-#define CPU_SPEED       10 // For simulation
+// #define CPU_SPEED       10 // For simulation
+// #define UART_BAUDRATE   1 //for simulation
 
-// #define UART_BAUDRATE   115200
-#define UART_BAUDRATE   1 //for simulation
+#define CPU_SPEED       25000000 //For synthesis
+#define UART_BAUDRATE   57600 //for synthesis
 
 #define UART_IN_TEST    0
 #define GPIO_TEST       1
