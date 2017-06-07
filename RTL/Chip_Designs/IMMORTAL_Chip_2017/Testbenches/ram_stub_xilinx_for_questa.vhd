515f03e7f684d5a3be587710d712815a8040ef49
tsotnep@gmail.com <tsotnep@gmail.com>
2017-06-07 18:27:49 +0300

    stub file for questasim, so it will stop bulshitting about UNISIM libraries that are used in xilinx rams

---
+++ RTL/Chip_Designs/IMMORTAL_Chip_2017/Testbenches/ram_stub_xilinx_for_questa.vhd
@@ -0,0 +1,96 @@
+-- In Questasim simulation, instead of adding actual
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use work.mlite_pack.all;
+entity ram_0 is
+   generic(memory_type : string := "DEFAULT";
+           --Number of 8KB blocks of internal RAM, up to 64KB (1 to 8)
+           block_count : integer := 8);
+   port(clk               : in std_logic;
+        enable            : in std_logic;
+        reset             : in std_logic;
+        write_byte_enable : in std_logic_vector(3 downto 0);
+        address           : in std_logic_vector(31 downto 2);
+        data_write        : in std_logic_vector(31 downto 0);
+        data_read         : out std_logic_vector(31 downto 0));
+end; --entity ram
+
+architecture logic of ram_0 is
+begin
+end;
+
+
+
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use work.mlite_pack.all;
+entity ram_1 is
+   generic(memory_type : string := "DEFAULT";
+           --Number of 8KB blocks of internal RAM, up to 64KB (1 to 8)
+           block_count : integer := 8);
+   port(clk               : in std_logic;
+        enable            : in std_logic;
+        reset             : in std_logic;
+        write_byte_enable : in std_logic_vector(3 downto 0);
+        address           : in std_logic_vector(31 downto 2);
+        data_write        : in std_logic_vector(31 downto 0);
+        data_read         : out std_logic_vector(31 downto 0));
+end; --entity ram
+architecture logic of ram_1 is
+begin
+end;
+
+
+
+
+
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use work.mlite_pack.all;
+entity ram_2 is
+   generic(memory_type : string := "DEFAULT";
+           --Number of 8KB blocks of internal RAM, up to 64KB (1 to 8)
+           block_count : integer := 8);
+   port(clk               : in std_logic;
+        enable            : in std_logic;
+        reset             : in std_logic;
+        write_byte_enable : in std_logic_vector(3 downto 0);
+        address           : in std_logic_vector(31 downto 2);
+        data_write        : in std_logic_vector(31 downto 0);
+        data_read         : out std_logic_vector(31 downto 0));
+end; --entity ram
+architecture logic of ram_2 is
+begin
+end;
+
+
+
+
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use work.mlite_pack.all;
+entity ram_3 is
+   generic(memory_type : string := "DEFAULT";
+           --Number of 8KB blocks of internal RAM, up to 64KB (1 to 8)
+           block_count : integer := 8);
+   port(clk               : in std_logic;
+        enable            : in std_logic;
+        reset             : in std_logic;
+        write_byte_enable : in std_logic_vector(3 downto 0);
+        address           : in std_logic_vector(31 downto 2);
+        data_write        : in std_logic_vector(31 downto 0);
+        data_read         : out std_logic_vector(31 downto 0));
+end; --entity ram
+architecture logic of ram_3 is
+begin
+end;
