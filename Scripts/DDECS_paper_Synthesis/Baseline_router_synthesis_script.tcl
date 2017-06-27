f3b9b3277ada05a86ad18c352036c5accf02aab9
Behrad Niazmand <bniazmand@pralinee.pld.ttu.ee>
2017-06-27 18:28:15 +0300

    Baseline router synthesis results and script added

---
+++ Scripts/DDECS_paper_Synthesis/Baseline_router_synthesis_script.tcl
@@ -0,0 +1,10 @@
+remove_design -designs
+analyze -library WORK -format vhdl {/home/bniazmand/pc/clean_Bonfire/Bonfire/RTL/Router/credit_based/RTL/xbar.vhd /home/bniazmand/pc/clean_Bonfire/Bonfire/RTL/Router/credit_based/RTL/LBDR.vhd /home/bniazmand/pc/clean_Bonfire/Bonfire/RTL/Router/credit_based/RTL/FIFO_one_hot_credit_based.vhd /home/bniazmand/pc/clean_Bonfire/Bonfire/RTL/Router/credit_based/RTL/arbiter_out.vhd /home/bniazmand/pc/clean_Bonfire/Bonfire/RTL/Router/credit_based/RTL/arbiter_in.vhd /home/bniazmand/pc/clean_Bonfire/Bonfire/RTL/Router/credit_based/RTL/allocator.vhd /home/bniazmand/pc/clean_Bonfire/Bonfire/RTL/Router/credit_based/RTL/Router_32_bit_credit_based.vhd}
+elaborate ROUTER_CREDIT_BASED -architecture BEHAVIOR -library DEFAULT -parameters "DATA_WIDTH = 32, current_address = 5, Cx_rst = 15, NoC_size = 4" -update
+create_clock -name "clk" -period 10 -waveform { 0 5  }  { clk  }
+set_dont_touch reset
+# compile -exact_map -area_effort none 
+#compile -exact_map -area_effort low
+# compile -exact_map -area_effort medium
+compile -exact_map -area_effort high
+report_area 
--- Scripts/synthesis_scripts/Router_32_credit_based_and_components_synthesis_script.tcl
+++ Scripts/synthesis_scripts/Router_32_credit_based_and_components_synthesis_script.tcl
@@ -8,7 +8,7 @@ sh mkdir area
 
 # Read and analyze all vhdl files for credit-based flow control router (RTL codes)
 
-analyze -library WORK -format vhdl {../RTL/Router/credit_based/RTL/arbiter_in.vhd ../RTL/Router/credit_based/RTL/arbiter_out.vhd ../RTL/Router/credit_based/RTL/allocator.vhd ../RTL/Router/credit_based/RTL/FIFO_one_hot_credit_based.vhd ../RTL/Router/credit_based/RTL/LBDR.vhd ../RTL/Router/credit_based/RTL/Router_32_bit_credit_based.vhd ../RTL/Router/credit_based/RTL/xbar.vhd}
+analyze -library WORK -format vhdl {../../RTL/Router/credit_based/RTL/arbiter_in.vhd ../../RTL/Router/credit_based/RTL/arbiter_out.vhd ../../RTL/Router/credit_based/RTL/allocator.vhd ../../RTL/Router/credit_based/RTL/FIFO_one_hot_credit_based.vhd ../../RTL/Router/credit_based/RTL/LBDR.vhd ../../RTL/Router/credit_based/RTL/Router_32_bit_credit_based.vhd ../../RTL/Router/credit_based/RTL/xbar.vhd}
 
 ####################################################################################################
 
@@ -26,7 +26,7 @@ report_area > area/FIFO_one_hot_credit_based_area.txt
 ####################################################################################################
 
 # LBDR (XY routing, router ID 5, network size: 4x4 (2D Mesh))
-elaborate LBDR -architecture behavior -library DEFAULT -parameters "cur_addr_rst = 5, Rxy_rst = 60, Rxy_rst = 60, Cx_rst = 15, NoC_size = 4"
+elaborate LBDR -architecture behavior -library DEFAULT -parameters "cur_addr_rst = 5, Cx_rst = 15, NoC_size = 4"
 # clock period set to 10 ns
 create_clock -name "clk" -period 10 -waveform { 0 5  }  { clk  }
 # reset signal
@@ -94,7 +94,7 @@ report_area > area/XBAR_area.txt
 ####################################################################################################
 
 # Router (credit-based flow control) (32 bit data width, XY routing, router ID 5, network size: 4x4 (2D Mesh))
-elaborate router_credit_based -architecture behavior -library DEFAULT -parameters "DATA_WIDTH = 32, current_address = 5, Rxy_rst = 60, Cx_rst = 15, NoC_size = 4"
+elaborate router_credit_based -architecture behavior -library DEFAULT -parameters "DATA_WIDTH = 32, current_address = 5, Cx_rst = 15, NoC_size = 4"
 # clock period set to 10 ns
 create_clock -name "clk" -period 10 -waveform { 0 5  }  { clk  }
 # reset signal
