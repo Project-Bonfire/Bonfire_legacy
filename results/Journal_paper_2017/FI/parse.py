813280aaecf36eaaf49e446dc0ab65fe21dd9e4c
siavooshpayandehazad <siavooshpayandeh@gmail.com>
2017-07-03 12:14:20 +0300

    updating journal paper results

--- results/Journal_paper_2017/FI/parse.py
+++ results/Journal_paper_2017/FI/parse.py
@@ -8,18 +8,19 @@ for i in range(0, 6):
 		#print x[0] 
 		for filename in os.listdir(x[0]):
 			if ".txt" in filename:
-				name = x[0][2:]+"/"+filename
-				file = open(name, "r")
-				if x[0][2:].split("/")[1] == str(i):
+				if filename != "info.txt":
+					name = x[0][2:]+"/"+filename
+					file = open(name, "r")
+					if x[0][2:].split("/")[1] == str(i):
 
-					for line in file:
-						split_list =  line.split()
-						# print line
-						if "sent" in line: 
-							packet_sent = split_list[2]
-							flit_sent = split_list[5]
-						if "lost" in line: 
-							packet_lost = split_list[2]
-							flit_lost = split_list[5]
-					print  x[0][2:].split("/")[0].split("_")[1],  "\t\t", filename.split("_")[2], "\t\t",   
-					print flit_sent, "\t\t", flit_lost, "\t\t", packet_sent, "\t\t", packet_lost
+						for line in file:
+							split_list =  line.split()
+							# print line
+							if "sent" in line: 
+								packet_sent = split_list[2]
+								flit_sent = split_list[5]
+							if "lost" in line: 
+								packet_lost = split_list[2]
+								flit_lost = split_list[5]
+						print  x[0][2:].split("/")[0].split("_")[1],  "\t\t", filename.split("_")[2], "\t\t",   
+						print flit_sent, "\t\t", flit_lost, "\t\t", packet_sent, "\t\t", packet_lost
