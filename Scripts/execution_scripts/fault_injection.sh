6b8c08ac8421ed6b35435dccd5aadebd25190954
siavooshpayandehazad <siavooshpayandeh@gmail.com>
2017-07-03 11:53:27 +0300

    updated FI script for Journal paper. also documentation about the experiment

--- Scripts/execution_scripts/fault_injection.sh
+++ Scripts/execution_scripts/fault_injection.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# we assume that we are in this path: Bonfire/Scripts/scripts_for_papers
-# TODO: update latency script to also handle FI cases!
+# we assume that we are in this path: Bonfire/Scripts/execution_scripts
 
 PL=8
-# check if there is a 
+# check if there is an output folder
 if [ ! -d "output" ]; then
     mkdir output
 else
@@ -15,12 +14,15 @@ fi
 for j in 0 1 2 3 4 5 
 do
 	mkdir output/$j
+	echo "made folder: output/$j"
 	for i in 0.005 0.01 0.015 0.02 0.025 0.03
 	  do 
 	     echo "running simulate.py for PIR: $i "
 	     # here we run the tool...
+	     # the -lat key is used just to run the tool without gui! this should be later added to the simulate script
 	     python ../../simulate.py -D 2 -credit_based_FC -packet_drop -FC -FI -NI_Test -SHMU -Rand $i -PS $PL $PL -sim 10000 -end 11000 -lat
 	     # copy the results into output folder
+	     echo "copying tmp/simul_temp/stats.txt to output/$j/latency_PIR\_$i\_PL\_$PL.txt"
 	     cp ../../tmp/simul_temp/stats.txt output/$j/latency_PIR\_$i\_PL\_$PL.txt
 	 done
 done
