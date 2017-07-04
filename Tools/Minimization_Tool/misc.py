610ea65bedf0db03bcafa53038ac4162b26df319
Siavoosh Payandeh Azad <siavooshpayandeh@gmail.com>
2017-07-04 15:05:46 +0300

    removed Handshaking related files

--- Tools/Minimization_Tool/misc.py
+++
@@ -1,9 +0,0 @@
-# copyright 2016 Siavoosh Payandeh Azad and Behrad Niazmand
-
-
-def name_string_generator(list_of_checkers):
-    name_string = ""
-    for i in sorted(list_of_checkers):
-        name_string += str(i)+"_"
-    name_string = name_string[:len(name_string)-1]
-    return name_string
