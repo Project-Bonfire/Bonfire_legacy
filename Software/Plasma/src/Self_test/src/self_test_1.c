025ef0aef7c287c6618603c561e9b26149d9cb09
Karl Janson <karl.janson@ati.ttu.ee>
2017-08-28 19:24:19 +0300

    self_test program now compiles again

--- Software/Plasma/src/Self_test/src/self_test_1.c
+++ Software/Plasma/src/Self_test/src/self_test_1.c
@@ -1,9 +1,10 @@
 #include "ni.h"
 #include "packets.h"
+#include "self_test.h"
 #include "plasma.h"
 #include "uart.h"
 #include "test_plasma.h"
-#include "ni_test.h"
+// #include "ni_test.h"
 
 
 #define MY_ADDR     1
@@ -22,13 +23,13 @@ int main(int argc, char const *argv[]) {
         {
             flit = ni_read();
         }else if((ni_read_flags() & NI_WRITE_MASK) == 0 && packet_counter < 100)
-        {   
-            flit_number ++; 
+        {
+            flit_number ++;
             if (flit_number == 1){
                 ni_write(build_header(destination, 3));
-                destination ++; 
+                destination ++;
                 if (destination == MY_ADDR){
-                    destination ++; 
+                    destination ++;
                 }
                 if (destination == 4){
                     destination = 0;
@@ -39,7 +40,7 @@ int main(int argc, char const *argv[]) {
                 packet_counter ++;
             }else{
                 ni_write(0b1111111111111111111111111111);
-            }    
+            }
         }
         if (packet_counter >= 100){
             while ((ni_read_flags() & NI_READ_MASK) == 0){
@@ -49,7 +50,7 @@ int main(int argc, char const *argv[]) {
             break;
         }
     }
- 
+
     /* Run CPU test */
     test_plasma_funcitons();
     return 0;
--- Software/Plasma/src/Self_test/src/self_test_2.c
+++ Software/Plasma/src/Self_test/src/self_test_2.c
@@ -1,16 +1,17 @@
 #include "ni.h"
 #include "packets.h"
+#include "self_test.h"
 #include "plasma.h"
 #include "uart.h"
 #include "test_plasma.h"
-#include "ni_test.h"
+// #include "ni_test.h"
 
 
 #define MY_ADDR     2
 #define DST_ADDR    3
 
 int main(int argc, char const *argv[]) {
-    
+
     unsigned destination = 0;
     unsigned flit_number = 0;
     unsigned flit;
@@ -24,13 +25,13 @@ int main(int argc, char const *argv[]) {
         {
             flit = ni_read();
         }else if((ni_read_flags() & NI_WRITE_MASK) == 0 && packet_counter < 100)
-        {   
-            flit_number ++; 
+        {
+            flit_number ++;
             if (flit_number == 1){
                 ni_write(build_header(destination, 3));
-                destination ++; 
+                destination ++;
                 if (destination == MY_ADDR){
-                    destination ++; 
+                    destination ++;
                 }
                 if (destination == 4){
                     destination = 0;
@@ -41,7 +42,7 @@ int main(int argc, char const *argv[]) {
                 packet_counter ++;
             }else{
                 ni_write(0b1111111111111111111111111111);
-            }    
+            }
         }
         if (packet_counter >= 100){
             while ((ni_read_flags() & NI_READ_MASK) == 0){
@@ -51,7 +52,7 @@ int main(int argc, char const *argv[]) {
             break;
         }
     }
-    
+
     /* Run CPU test */
     test_plasma_funcitons();
     return 0;
